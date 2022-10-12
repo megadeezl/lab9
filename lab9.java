91c91dc8e55b710da5cc0f63e8519bca4a2d0204
Robinson-erc <109825086+Robinson-erc@users.noreply.github.com>
2022-10-12 18:59:15 -0400

    Add files via upload

--- lab9.java
+++ lab9.java
@@ -7,13 +7,18 @@
  */
 import javax.swing.*;
 import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class lab9
 {
-    public static void main(String args[])
+    public static void main(String args[]) 
     {
         P1();
         P2();
+        P3();
     }
     static void P1()
     {
@@ -22,14 +27,19 @@ public class lab9
         String Last = JOptionPane.showInputDialog("Enter your last name");
         JOptionPane.showMessageDialog(null, "Hello "+First+" "+Middle+" "+Last); 
     }
-   static void P2()
+   static void P2() 
     {
         /*
          * Need to get temperature from user in fahrenheit and convert
          * to celsius. Need to first get user input to name file they
-         * want the array to go to see project 1 P3().
+         * want the array to go to see project 1 P2().
          */
+        String fileName =JOptionPane.showInputDialog("Enter file name");
+        File fileObject = new File(fileName);
+        PrintWriter outFile = null;
+        
         String sizeofarray = JOptionPane.showInputDialog("enter size of array");
+       
         int num1 = Integer.parseInt(sizeofarray);
         Double [] TempArray = new Double [num1];
         //JOptionPane.showMessageDialog(null, "Enter elements of array");
@@ -37,9 +47,16 @@ public class lab9
             int TempArrayX=0;
             TempArrayX=Integer.parseInt(JOptionPane.showInputDialog(null, "Enter elements of array"));
            // double TempArrayX[] = Double.parseDouble();
-            JOptionPane.showMessageDialog(null,""+ TempArrayX+" degrees farenheight");            
+            JOptionPane.showMessageDialog(null,""+ TempArrayX+" degrees farenheight");
+            try{
+                 outFile= new PrintWriter(new FileOutputStream(fileObject));
+            }catch(FileNotFoundException e){
+                
+            }
         }
+       
         Double [] TempArrayXCopy = new Double[num1];
+        
         
         
         
@@ -47,6 +64,7 @@ public class lab9
     static void P3()
     {
         
+         int num1= Integer.parseInt(JOptionPane.showInputDialog("Enter a positive number"));
     }
 
 }
